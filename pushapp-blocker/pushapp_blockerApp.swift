758839fbfb84a398cbf9e07881ca5a339c195e38
stepkurniawan <stepkurniawan@gmail.com>
2026-09-01 14:03:21 +0200

    Initial Commit

---
+++ pushapp-blocker/pushapp_blockerApp.swift
@@ -0,0 +1,32 @@
+//
+//  pushapp_blockerApp.swift
+//  pushapp-blocker
+//
+//  Created by stephen on 01.09.26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct pushapp_blockerApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ pushapp-blockerTests/pushapp_blockerTests.swift
@@ -0,0 +1,19 @@
+//
+//  pushapp_blockerTests.swift
+//  pushapp-blockerTests
+//
+//  Created by stephen on 01.09.26.
+//
+
+import Testing
+@testable import pushapp_blocker
+
+struct pushapp_blockerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
